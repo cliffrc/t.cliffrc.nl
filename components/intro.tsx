24cfdd13b2bea85c069f15952f410b87b7fe9d6d
Cliff Cunnington <cliff@10strata.com>
2023-11-05 08:30:01 +0100

    Add parameters to CV download and _blank target to links

--- components/intro.tsx
+++ components/intro.tsx
@@ -71,6 +71,7 @@ export default function Intro() {
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition"
           href="/CV.pdf"
+          download
         >
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
@@ -78,12 +79,14 @@ export default function Intro() {
         <a
           className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full outline-none  focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950  active:scale-105 transition cursor-pointer border border-black/10"
           href="https://linkedin.com/"
+          target="_blank"
         >
           <BsLinkedin />
         </a>
         <a
           className=" bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
           href="https://github.com/"
+          target="_blank"
         >
           <FaGithubSquare />
         </a>
